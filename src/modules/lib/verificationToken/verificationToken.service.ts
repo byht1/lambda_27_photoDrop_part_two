4ed718be5afc4c5840b23915f78804531a6fa0a8
Vitalii Dykhal <vitalikbyht1@gmail.com>
2023-05-27 20:00:34 +0300

    fix

--- src/modules/lib/verificationToken/verificationToken.service.ts
+++ src/modules/lib/verificationToken/verificationToken.service.ts
@@ -16,20 +16,14 @@ export class VerificationTokenService implements IVerificationTokenService {
     const code = this.generateVerificationCode()
     const payload: TPayloadVerificationToken = { code, attemptNumber }
     const token = jwt.sign(payload, this.tokenKey, { expiresIn: this.expTime })
-    console.log('🚀  createToken  this.tokenKey:', this.tokenKey)
-    console.log('🚀  createToken this.expTime:', this.expTime)
     return { token, code }
   }
 
   verify: TVerifyFn = async (token, errorFn) => {
-    console.log('🚀  verify  token:', token)
-    console.log('🚀  verify  this.tokenKey:', this.tokenKey)
-    console.log('🚀  verify  this.expTime:', this.expTime)
     try {
       const payload = jwt.verify(token, this.tokenKey) as TPayloadVerificationToken
       return payload
     } catch (error) {
-      console.log('🚀  VerificationTokenService  error:', error)
       if (errorFn) await errorFn()
       throw createError(403, messageError.invalidVerificationCodeTime)
     }
