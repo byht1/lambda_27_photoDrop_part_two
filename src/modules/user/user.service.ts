3fe96b6c751cefe542fd753dcce487d4b3e43f87
Vitalii Dykhal <vitalikbyht1@gmail.com>
2023-06-11 14:55:16 +0300

    fix

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,14 @@
 import { S3Service } from 'AWS'
 import { IUserService, TAddSelfieFn, TSetUserDataFn } from './type'
 import { UsersRepository } from 'db/repository'
+import { TelegramBotService, VerificationTokenService } from 'modules/lib'
+import { TUpdateData } from 'db/repository/users/type'
 
 export class UserService implements IUserService {
   private s3Service = new S3Service()
   private userModel = new UsersRepository()
+  private telegramService = new TelegramBotService()
+  private verificationTokenService = new VerificationTokenService()
 
   addSelfie: TAddSelfieFn = async (files, userId) => {
     const URLs = files.map((filename, i) => {
@@ -17,9 +21,18 @@ export class UserService implements IUserService {
   }
 
   setUserData: TSetUserDataFn = async (userId, { phoneNumber, ...params }) => {
+    const newParamsObj: Partial<TUpdateData> = { ...params }
+    if (phoneNumber) {
+      const { token: verificationToken, code: verificationCode } =
+        this.verificationTokenService.createToken(1)
+
+      await this.telegramService.sendVerificationCode(phoneNumber, verificationCode)
+
+      newParamsObj.verificationToken = verificationToken
+      newParamsObj.phone = phoneNumber
+    }
     const { id, name, phone, avatar, email } = await this.userModel.setUserData(userId, {
-      ...params,
-      ...(phoneNumber ? { phone: phoneNumber } : {}),
+      ...newParamsObj,
     })
 
     return { id, name, phone, avatar, email }
